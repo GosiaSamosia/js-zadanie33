29bdf70e5d5d14a2d0941e2db85d4eb395189b14
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-14 11:02:45 +0200

    wstawianie paragrafu za pomocą przycisku

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var paragraf1 = document.getElementById("opis");
+var przycisk1 = document.querySelector("button");
+document.querySelector("button").onclick = function() {
+    paragraf1.textContent = "Akademia108";
+}
